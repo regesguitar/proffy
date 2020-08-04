64e68a5b19d253bd0a990cc0c70b5abc2ec36393
bruno-reges <bruno.reges@hp.com>
2020-08-04 01:40:15 -0300

    adding_design

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-
+import Landing from './pages/Landing'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Landing />
   );
 }
 
